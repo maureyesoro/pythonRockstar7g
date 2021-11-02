d37f7ed35ece6766707bb54aafb90feba46bfa4f
maureyesoro <mauricio.reyes@enroutesystems.com>
2021-11-02 17:19:48 -0600

    commit 4

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,17 @@
 print('hello world')
 
-for i in range (1,100,2):
-    print(i)
+# for i in range (2,100,2):
+  #  print(i)
+
+#for i in range (1,100,2):
+ #   print(i)
+
+
+for i in range(1,100):
+    primo = True
+    for j in range (2,9):
+        if i % j == 0 and i != j:
+            primo = False
+            #print(i, j, 'no primo')
+    if primo:
+        print('primo', i)
